5c0e40f2290da24c93fc543151208a85dcbc34dd
Jeeva <jeeva@eastpoint.co.uk>
2015-12-18 22:48:57 +0000

    Initial Setup - using CoreBluetooth - LE (Low Energy)

--- BlueToothMan/ViewController.swift
+++ BlueToothMan/ViewController.swift
@@ -7,19 +7,61 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CBCentralManagerDelegate {
 
+    var centralManager : CBCentralManager!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.centralManager = CBCentralManager(delegate: self, queue: nil)
+        
+        
+        
+    }
+    
+    func StartScanning(){
+        self.centralManager.scanForPeripheralsWithServices(nil, options: nil)
+        
+    }
+    
+    func centralManager(central: CBCentralManager, didDiscoverPeripheral peripheral: CBPeripheral, advertisementData: [String : AnyObject], RSSI: NSNumber) {
+        
+        print("haha")
+        print("******************")
+        print("Name   : \(peripheral.name)")
+        print("UUID   : \(peripheral.identifier.UUIDString)")
+        print("Advert : \(advertisementData)")
+        print("RSSI   : \(RSSI)")
+        print("******************")
+    }
+    
+    func centralManagerDidUpdateState(central: CBCentralManager) {
+        
+        print("We are in Central Manager did update state")
+        if central.state == CBCentralManagerState.PoweredOn {
+            print("Bluetooth is ON")
+            StartScanning()
+        }else
+        {
+            let alertViewController = UIAlertController(title: "Bluetooth Man", message: "Make sure that your bluetooth is on", preferredStyle: UIAlertControllerStyle.Alert)
+            let action = UIAlertAction(title: "Bluetooth Not working", style: UIAlertActionStyle.Default, handler: { (act  : UIAlertAction) -> Void in
+                self.dismissViewControllerAnimated(true, completion: nil)
+            })
+            alertViewController.addAction(action)
+            self.presentViewController(alertViewController, animated: true, completion: nil)
+            print("Bluetooth is OFF")
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
+    
 
 }
 
