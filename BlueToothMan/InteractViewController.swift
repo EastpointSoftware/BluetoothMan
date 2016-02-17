09c98188b795dcaec00b1e6c8399c99b7e972f8d
Jeeva <jeeva@eastpoint.co.uk>
2016-02-17 19:22:29 +0000

    Added some comment to define the PID's used.

--- BlueToothMan/InteractViewController.swift
+++ BlueToothMan/InteractViewController.swift
@@ -63,22 +63,18 @@ class InteractViewController : UIViewController {
     
     func readServiceCharacteristics() {
 
-        self.writeCharacteristic = nil
-        self.readCharacteristic = nil
-        
-        
-        
         let readCharacteristic  : CBUUID = CBUUID(string: "FFF1")
         let writeCharacteristic : CBUUID = CBUUID(string: "FFF2")
         var error : NSError?
     
-        for var characteristic in service.characteristics {
-            
+        self.writeCharacteristic = nil
+        self.readCharacteristic = nil
+        
+        for  characteristic in service.characteristics {
             
             if characteristic.uuid.isEqual(readCharacteristic) {
-                
+         
                 self.readCharacteristic = characteristic
-                
                 LogMessage("Read Characteristic found ")
                 
             }else if characteristic.uuid.isEqual(writeCharacteristic) {
@@ -87,15 +83,15 @@ class InteractViewController : UIViewController {
                 LogMessage("Write Characteristic found ")
             }
             
-            
-            
         }
-        
         
         
         if self.writeCharacteristic != nil && self.readCharacteristic != nil {
         
-            
+            //Construct Initial Command to get a list of vehicle supported PIDs 
+            // PID - 01 =>
+            // PID - 00 => Most supported pids to get the list of supported pids
+            // Online Reference : https://learn.sparkfun.com/tutorials/obd-ii-uart-hookup-guide
             
             var data: NSData = NSData(bytes: [0x01, 0x00] as [UInt8], length: 2)
             
@@ -103,7 +99,6 @@ class InteractViewController : UIViewController {
             
             self.readCharacteristic.service?.peripheral?.didUpdateValueForCharacteristic(self.readCharacteristic.cbCharacteristic , error : error)
 
-            
             error = nil
             
             self.readCharacteristic.didUpdate(error)
@@ -115,6 +110,11 @@ class InteractViewController : UIViewController {
                  LogMessage( "Updated Value  \(hexString) ")
             }
             
+            
+            // PID - 01 => Current Data
+            // PID - 0c => RPM data
+            
+            
             data = NSData(bytes: [0x01, 0x0c] as [UInt8], length: 2)
             
             self.writeCharacteristic.service?.peripheral?.writeValue(data, forCharacteristic: self.writeCharacteristic)
@@ -122,10 +122,12 @@ class InteractViewController : UIViewController {
            
             
         }
+        
+    }
     
-        
-        
-}
+    
+    
+    
 
 }
 
